151214a15c2495cfe3d47fd49c41ee3a67eab742
vagrant <vagrant@localhost.localdomain>
2020-10-14 11:53:43 +0000

    [Sfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'home/top' => 'homes#top'
+root to: 'homes#top'
 get 'home/about' => 'homes#about'
 get 'genre_search' => 'videos#genre_search' #ジャンル検索
 resources :videos, only: [:new, :create, :show, :edit, :update, :destroy] do
